7cab006b10b62c057001c140bc86fdd7034c5c7f
AlineSug <aline_spinelly@hotmail.com>
2024-09-05 13:33:32 +0900

    initial commit

---
+++ loginn.py
@@ -0,0 +1,28 @@
+while True:
+    login = input("Type username:")
+    passw = input("Type your password:")
+    confirm = input("Confirme your password:")
+    if passw in confirm:
+        print("Subscription conclude")
+        print("-------------------")
+        break
+    else:
+        print("Error in password confirmation")
+
+
+user = login
+password = passw
+
+
+def logar_in(user, password):
+    log1 = input("Login:")
+    pass1 = input("Password:")
+    if user == log1 and password == pass1:
+        print("Welcome to your Account")
+        print("--------( ^ _ ^ )-----------")
+    else:
+        print("Access denied")
+        print("*********( -_- )*********")
+
+
+logar_in(user, password)
